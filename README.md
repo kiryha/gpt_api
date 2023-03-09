6557f87117023c34cbde62692f3ade2e9ed6a219
Kiryha <kiryha@gmail.com>
2023-03-09 18:31:33 -0500

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# gpt_api
-Chat GPT API R&amp;D
+# Chat GPT R&D
+
+#### Getting Started with Chat GPT Python API
+- Install Python 3
+- Install openai library: pip install openai
+- Create account on https://platform.openai.com/overview
+- Setup payment method and get API key
+- Write python code
--- gpt_api/gpt_api.py
+++ gpt_api/gpt_api.py
@@ -3,9 +3,6 @@ from open_api_key import OPEN_API_KEY
 
 openai.api_key = OPEN_API_KEY
 
-# completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": '2+3=?'}])
-# print(completion.choices[0].message.content)
-
 messages = []
 message = input('Chat GPT: How can I assist you today?')
 if message:
@@ -19,5 +16,3 @@ while True:
         reply = completion.choices[0].message.content
         print(f'Chat GPT: {reply}')
         messages.append({'role': 'assistant', 'content': reply})
-
-
