86210f7bcd4ecfceabf1464b1866a8ee74eca4fd
Kiryha <kiryha@gmail.com>
2023-03-09 17:33:17 -0500

    Hello World

--- gpt_api/gpt_api.py
+++ gpt_api/gpt_api.py
@@ -1,3 +1,9 @@
 import openai
-import open_api_key
+from open_api_key import OPEN_API_KEY
 
+openai.api_key = OPEN_API_KEY
+
+message = "What is the distance to sun in kilometers?"
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": message}])
+
+print(completion.choices[0].message.content)
